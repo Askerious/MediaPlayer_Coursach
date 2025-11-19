6025f1952d1f5390f83031ad9a4c5f3d321f6d95
Askerious <mikhaylov-kot@yandex.ru>
2025-11-19 07:41:58 +0700

    mainWindow

---
+++ Domain/AudioTrack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class AudioTrack
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public bool IsFavorite { get; set; }
+        public string FilePath { get; set; }
+
+        public AudioTrack (int id, string title, int duration, bool isFavorite, string filePath)
+        {
+            Id = id;
+            Title = title;
+            Duration = duration;
+            IsFavorite = isFavorite;
+            FilePath = filePath;
+        }
+    }
+}
---
+++ UI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Domain;
+
+namespace UI
+{
+    public partial class MainWindow : Window
+    {
+        public MediaPlayer player = new MediaPlayer();
+        public ObservableCollection<AudioTrack> tracks = new ObservableCollection<AudioTrack>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void PlayMedia(object sender, EventArgs e)
+        {
+            player.Play();
+
+
+        }
+
+        public void LoadTrack(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == true)
+            {
+                player.Open(new Uri(ofd.FileName));
+                AudioTrack track = new AudioTrack(1, "test", 45, false, "gavno");
+                tracks.Add(track);
+                dataGrid.ItemsSource = tracks;
+            }
+        }
+    }
+}
